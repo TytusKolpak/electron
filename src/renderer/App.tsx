ae15bd668f61a2121a15f5c49b840a458b09d0d5
TytusKolpak <tytus.kolpak.71@gmail.com>
2023-10-04 12:57:45 +0200

    Build a board

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,39 +1,20 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
 import './App.css';
+import { useState } from 'react';
 
 function Hello() {
+  const [count, setCount] = useState(0);
+  const tileAmount = 30;
+  const divElements = [];
+
+  for (let i = 1; i <= tileAmount; i++) {
+    divElements.push(<div key={i}>Div {i}</div>);
+  }
+
   return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+    <div id='mainField'>
+      <div className="parentBoard">
+      {divElements}
       </div>
     </div>
   );
